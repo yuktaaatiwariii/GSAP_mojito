de72d55926e769623a8178abc9ff7c1b00d5deec
yuktaaatiwariii <yuktatiwari0@gmail.com>
2026-01-16 21:27:41 +0530

    implement cocktails sestion

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { gsap } from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
+import Cocktails from './components/Cocktails.jsx';
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,7 +13,7 @@ const App = () => {
   <main>
   <Navbar />
   <Hero />
-  
+  <Cocktails/>
   </main>
 )
 }
